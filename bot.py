0808f17a1099456c5bf3f86a28f636c250d05d1e
Jonathan Erroneous <jon@e.rroneo.us>
2018-06-30 20:05:08 +0100

    Simplified response system to run off dictionary

--- bot.py
+++ bot.py
@@ -7,20 +7,24 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def respond_betanet(bot, update):
-	update.message.reply_text("🎉🎉🎉🎉🎉 IT'S SOON GUYS 🎉🎉🎉🎉🎉")
+api_key = "APIKEY"
+responses = {
+	"tezosbetanet"		:	"🎉🎉🎉🎉🎉 BETANET WILL BE TODAY! 🎉🎉🎉🎉🎉",
+	"tezostothemoon"	:	"ꜩ🚀 -> 🌕 @ 🎉🎉🎉🎉🎉 ANY TIME NOW 🎉🎉🎉🎉🎉"
+}
 
-def respond_tothemoon(bot, update):
-	update.message.reply_text("ꜩ🚀 -> 🌕 @ 🎉🎉🎉🎉🎉 ANY TIME NOW 🎉🎉🎉🎉🎉")
+def respond(bot, update):
+	command = update.effective_message['text'].split('/')[1].split(' ')[0]
+	if command in responses:
+		update.message.reply_text(responses[command])
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 def main():
-    updater = Updater("APIKEY")
+    updater = Updater(api_key)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("tezosbetanet", respond_betanet))
-    dp.add_handler(CommandHandler("tezostothemoon", respond_tothemoon))
+    dp.add_handler(CommandHandler(responses.keys(), respond))
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
