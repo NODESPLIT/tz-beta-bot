5428ebf5266569baf249050f8f098e8bf48032f3
Jonathan Erroneous <jon@e.rroneo.us>
2018-06-30 21:02:48 +0100

    Added command to get price from coinmarketcap

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # tz-beta-bot
 Python telegram bot for celebrating betanet!  
   
-/tezosbetanet - Celebrate the betanet launch with spam!  
-/tezostothemoon - Tezos to the moon for all the when mooners  
+### Command List:  
+betanet - Celebrate the betanet launch with spam!  
+tothemoon - Tezos to the moon for all the when mooners  
+price - Get a current price for Tezos as well as % price change
   
 Come and try it out here: https://t.me/tezosplatform
--- bot.py
+++ bot.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import urllib.request, json, time
 from telegram.ext import Updater, CommandHandler
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+last_price_data = {}
+last_price_check = -1
+
+def price():
+	global last_price_data, last_price_check
+	if time.time() - last_price_check >= 60:
+		with urllib.request.urlopen("https://api.coinmarketcap.com/v2/ticker/2011/") as url:
+			data = json.loads(url.read().decode())
+			last_price_data = data
+			last_price_check = time.time()
+	else:
+		data = last_price_data
+	return "XTZ/USD:	$"+str(round(data["data"]["quotes"]["USD"]["price"], 2))+"\n1hr:		"+str(data["data"]["quotes"]["USD"]["percent_change_1h"])+"%\n24hr:		"+str(data["data"]["quotes"]["USD"]["percent_change_24h"])+"%\n7d:		"+str(data["data"]["quotes"]["USD"]["percent_change_7d"])+"%"
+
 
 api_key = "APIKEY"
 responses = {
-	"tezosbetanet"		:	"🎉🎉🎉🎉🎉 BETANET WILL BE TODAY! 🎉🎉🎉🎉🎉",
-	"tezostothemoon"	:	"ꜩ🚀 -> 🌕 @ 🎉🎉🎉🎉🎉 ANY TIME NOW 🎉🎉🎉🎉🎉"
+	"betanet"		:		"🎉🎉🎉🎉🎉 BETANET HAS LAUNCHED! https://tezosfoundation.ch/news/tezos-betanet-launch/ 🎉🎉🎉🎉🎉",
+	"tothemoon"		:		"ꜩ🚀 -> 🌕 @ 🎉🎉🎉🎉🎉 ANY TIME NOW 🎉🎉🎉🎉🎉",
+	"price"			:		price
 }
 
 def respond(bot, update):
-	command = update.effective_message['text'].split('/')[1].split(' ')[0].split('@')[0]
+	global responses
+	command = update.effective_message["text"].split("/")[1].split(" ")[0].split("@")[0]
 	if command in responses:
-		update.message.reply_text(responses[command])
+		if callable(responses[command]):
+			update.message.reply_text(responses[command]())
+		else:
+			update.message.reply_text(responses[command])
 
 def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    logger.warning("Update '%s' caused error '%s'", update, error)
 
 def main():
     updater = Updater(api_key)
@@ -29,5 +50,5 @@ def main():
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
