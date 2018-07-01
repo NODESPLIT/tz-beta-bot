fd8f5862636986e02af20d8a75ea044a77a53d3a
Jonathan Erroneous <jon@e.rroneo.us>
2018-07-01 10:01:00 +0100

    Added aliases to commands list + Made API key load in from file to make it easier to deploy + Updated claim command guide and removed link from betanet command

--- bot.py
+++ bot.py
@@ -22,18 +22,26 @@ def price():
 		data = last_price_data
 	return "XTZ/USD:	$"+str(round(data["data"]["quotes"]["USD"]["price"], 2))+"\n1hr:		"+str(data["data"]["quotes"]["USD"]["percent_change_1h"])+"%\n24hr:		"+str(data["data"]["quotes"]["USD"]["percent_change_24h"])+"%\n7d:		"+str(data["data"]["quotes"]["USD"]["percent_change_7d"])+"%"
 
+key_file = open("key", "r+")
+api_key = key_file.read().strip()
 
-api_key = "APIKEY"
 responses = {
-	"betanet"		:		"🎉🎉🎉🎉🎉 BETANET HAS LAUNCHED! https://tezosfoundation.ch/news/tezos-betanet-launch/ 🎉🎉🎉🎉🎉",
+	"betanet"		:		"🎉🎉🎉🎉🎉 BETANET HAS LAUNCHED! 🎉🎉🎉🎉🎉",
 	"tothemoon"		:		"ꜩ🚀 -> 🌕 @ 🎉🎉🎉🎉🎉 ANY TIME NOW 🎉🎉🎉🎉🎉",
-	"claim"			:		"\nYou can now claim your Tezos using TezBox: https://tezbox.github.io/\nThis has a GUI for claiming and is a fully featured wallet on betanet!\n\n🚨🚨🚨 If you don't feel safe claiming this early it's probably best to wait for betanet to mature 🚨🚨🚨\n\n1. Click \"Access the Online Wallet\"\n2. Click \"RESTORE TEZBOX\"\n3. Click \"FUNDRAISER WALLET\"\n4. Enter your contribution details and click \"RESTORE TEZBOX\"\n5. If you have zero balances wait for a while for your wallet to sync",
+	"claim"			:		"\nYou can now claim your Tezos using TezBox: https://tezbox.github.io/\nThis has a GUI for claiming and is a fully featured wallet on betanet!\n\n🚨🚨🚨 If you don't feel safe claiming this early it's probably best to wait for betanet to mature 🚨🚨🚨\n\n1. Click \"Access the Online Wallet\"\n2. Click \"RESTORE TEZBOX\"\n3. Click \"FUNDRAISER WALLET\"\n4. Enter your contribution details and click \"RESTORE TEZBOX\"\n5. If you have zero balances wait for a while for your wallet to sync\n\nIf you still get 0 balance after waiting manually activate using: https://stephenandrews.github.io/activatez/",
 	"price"			:		price
 }
 
+aliases = {
+	"tezosbetanet"		:	"betanet",
+	"tezostothemoon"	:	"tothemoon"
+}
+
 def respond(bot, update):
-	global responses
+	global responses, aliases
 	command = update.effective_message["text"].split("/")[1].split(" ")[0].split("@")[0]
+	if command in aliases:
+		command = aliases[command]
 	if command in responses:
 		if callable(responses[command]):
 			update.message.reply_text(responses[command]())
@@ -46,7 +54,7 @@ def error(bot, update, error):
 def main():
     updater = Updater(api_key)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler(responses.keys(), respond))
+    dp.add_handler(CommandHandler(list(responses.keys()) + list(aliases.keys()), respond))
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
